9b74d41543c9cdb7214ae41ceca4cc67355628d3
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-07 14:49:16 +0200

    Add swagger

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask_swagger_ui import get_swaggerui_blueprint
 app = Flask(__name__)
 
 SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
-API_URL = 'http://petstore.swagger.io/v2/swagger.json'  # Our API url (can of course be a local resource)
+API_URL = '/static/swagger.json'  # Our API url (can of course be a local resource)
 
 # Call factory function to create our blueprint
 swaggerui_blueprint = get_swaggerui_blueprint(
